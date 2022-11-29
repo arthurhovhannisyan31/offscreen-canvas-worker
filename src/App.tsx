d5948f8aabbd2098c6581298db4f0005441615d8
Artur Oganesian <arthurhovhannisyan31@gmail.com>
2022-11-29 21:59:13 +0400

    main clean up init files

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import diceIcon from "./assets/dice.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <img src={diceIcon} className="logo speed0" alt="dice icon" />
+      <img src={diceIcon} className="logo speed1" alt="dice icon" />
+      <img src={diceIcon} className="logo speed2" alt="dice icon" />
+      <img src={diceIcon} className="logo speed3" alt="dice icon" />
+      <img src={diceIcon} className="logo speed4" alt="dice icon" />
+      <h1>Offscreen canvas worker</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
