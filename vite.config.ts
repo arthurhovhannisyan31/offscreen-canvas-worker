68d18ae70ac428fa38b8933e0bb3e0b21c36f7af
Artur Oganesian <arthurhovhannisyan31@gmail.com>
2022-12-07 00:20:33 +0400

    ops(workers): update configs
    - add ts path resolutions
    - filter deleted files for eslint check on pre-commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 import htmlPlugin from "vite-plugin-html-config"
+import tsconfigPaths from "vite-tsconfig-paths"
 
 const htmlPluginOpt = {
   favicon: "/favicon.ico"
@@ -9,7 +10,8 @@ const htmlPluginOpt = {
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
+    tsconfigPaths(),
     htmlPlugin(htmlPluginOpt),
+    react(),
   ],
 })
