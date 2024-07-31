7dc4c2fb3b88636b0d8c957ff6ebd28c36323163
Arthur Hovhannisyan <43288945+arthurhovhannisyan31@users.noreply.github.com>
2024-07-31 20:00:13 +0400

    Update deps (#18)
    * update gh config
    
    * update deps

--- configs/vite/vite.config.ts
+++ configs/vite/vite.config.ts
@@ -18,7 +18,6 @@ export default defineConfig({
       "Cross-Origin-Opener-Policy": "same-origin",
       "Cross-Origin-Embedder-Policy": "require-corp",
     },
-    https: true
   },
   plugins: [
     tsconfigPaths(),
