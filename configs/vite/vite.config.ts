163170d40a3471927d41f55a474dc56d79942786
Arthur Hovhannisyan <arthurhovhannisyan31@gmail.com>
2024-09-24 14:45:57 +0400

    configure env variables for vite config

--- configs/vite/vite.config.ts
+++ configs/vite/vite.config.ts
@@ -1,6 +1,6 @@
 import basicSsl from "@vitejs/plugin-basic-ssl";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv, type ConfigEnv, type UserConfig } from "vite";
 import htmlPlugin from "vite-plugin-html-config";
 import tsconfigPaths from "vite-tsconfig-paths";
 
@@ -8,21 +8,28 @@ const htmlPluginOpt = {
   favicon: "/favicon.ico"
 };
 
-export default defineConfig({
-  build: {
-    sourcemap: true,
-    manifest: true,
-  },
-  server:{
-    headers:{
-      "Cross-Origin-Opener-Policy": "same-origin",
-      "Cross-Origin-Embedder-Policy": "require-corp",
+export default ({ mode }: ConfigEnv): UserConfig => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  console.log(process.env.VITE_ASSETS_PATH);
+
+  return defineConfig({
+    base: process.env.VITE_ASSETS_PATH,
+    build: {
+      sourcemap: true,
+      manifest: true,
     },
-  },
-  plugins: [
-    tsconfigPaths(),
-    htmlPlugin(htmlPluginOpt),
-    react(),
-    basicSsl()
-  ],
-});
+    server:{
+      headers:{
+        "Cross-Origin-Opener-Policy": "same-origin",
+          "Cross-Origin-Embedder-Policy": "require-corp",
+      },
+    },
+    plugins: [
+      tsconfigPaths(),
+      htmlPlugin(htmlPluginOpt),
+      react(),
+      basicSsl()
+    ],
+  });
+};
